5061647b3a0d1e33fbbb9c121669a8b43d61c6d5
buchhola <alexander.buchholz@ensae.fr>
2018-05-16 18:20:17 +0200

    rerun simulations

--- script_run_simulations.sh
+++ script_run_simulations.sh
@@ -3,6 +3,6 @@
 python run_simulations_server_student.py 'loop' 'full'
 #python run_simulations_server_normal.py 'loop' 'full'
 python run_simulations_server_log_cox.py 'loop' 'full'
-#python run_simulations_server_logit.py 'loop' 'full'
-#python run_simulations_server_probit.py 'loop' 'full'
+python run_simulations_server_logit.py 'loop' 'full'
+python run_simulations_server_probit.py 'loop' 'full'
 
