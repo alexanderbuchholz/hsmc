69402286a5284b64418395a54d6451cf5366bfe0
buchhola <alexander.buchholz@ensae.fr>
2018-05-18 11:48:47 +0200

    change prior for student

--- script_run_simulations.sh
+++ script_run_simulations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # run all simulations after another
-#python run_simulations_server_student.py 'loop' 'full'
+python run_simulations_server_student.py 'loop' 'full'
 #python run_simulations_server_normal.py 'loop' 'full'
 python run_simulations_server_log_cox.py 'loop' 'full'
 python run_simulations_server_logit.py 'loop' 'full'
